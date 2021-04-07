0a8d4aec1ad42658bd0e7e9ccbf881efafa01766
Danex2 <khadane.miller@gmail.com>
2021-04-07 00:38:59 -0400

    chore: copying code from old projects and adding more config

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,55 @@
+import NextAuth, { InitOptions, Session } from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+const options: InitOptions = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+  secret: process.env.SECRET,
+  session: {
+    jwt: false,
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
+  callbacks: {
+    redirect: async (url, baseUrl) => {
+      return url.startsWith(baseUrl)
+        ? Promise.resolve(url)
+        : Promise.resolve(baseUrl);
+    },
+    session: async (session: any, user: any) => {
+      session.user.id = user.id;
+
+      return Promise.resolve(session);
+    },
+  },
+};
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, options);
--- schema/index.ts
+++ schema/index.ts
@@ -1,9 +1,15 @@
 import { makeSchema } from "nexus";
 import * as QueryTypes from "./Query";
+import { nexusPrisma } from "nexus-plugin-prisma";
 import path from "path";
 
 const schema = makeSchema({
   types: [QueryTypes],
+  plugins: [
+    nexusPrisma({
+      experimentalCRUD: true,
+    }),
+  ],
   outputs: {
     typegen: path.join(process.cwd(), "generated/nexus-typegen.ts"),
     schema: path.join(process.cwd(), "generated/schema.graphql"),
