b25097422caded26b2a8bc26693c906d51a2a47c
Danex2 <khadane.miller@gmail.com>
2021-04-08 01:04:03 -0400

    feat: add signout functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,29 +3,58 @@ import { Dialog } from "@reach/dialog";
 import "@reach/dialog/styles.css";
 import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
-import { signIn } from "next-auth/client";
+import { signIn, useSession, signOut } from "next-auth/client";
 
 export const Navbar = () => {
   const [showDialog, setShowDialog] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
   const open = () => setShowDialog(true);
   const close = () => setShowDialog(false);
 
+  const [session] = useSession();
+
   return (
-    <nav className="py-4 bg-gray-100 shadow-md">
+    <nav className="py-4 bg-gray-100">
       <div className="flex items-center justify-between max-w-5xl mx-auto">
         <h1 className="text-3xl font-bold text-purple-700">NYA</h1>
         <ul className="flex space-x-3 font-semibold">
-          <li
-            className="text-purple-700 duration-150 opacity-75 cursor-pointer hover:opacity-100"
-            onClick={open}
-          >
-            Sign In
-          </li>
-          <li className="text-purple-700 duration-150 opacity-75 hover:opacity-100">
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
+          {session ? (
+            <>
+              <button
+                type="button"
+                aria-expanded={showDropdown}
+                className="relative"
+                onClick={() => setShowDropdown(!showDropdown)}
+              >
+                <img
+                  src={session?.user?.image}
+                  alt={`User avatar for ${session?.user?.name}`}
+                  className="w-8 h-8 rounded-full"
+                />
+              </button>
+              <div
+                className={`absolute top-[60px] right-[130px] bg-gray-100 shadow-md border border-purple-700 py-2 px-4 hover:bg-gray-300 duration-150 cursor-pointer ${
+                  showDropdown ? "block" : "hidden"
+                }`}
+              >
+                <p onClick={() => signOut()}>Sign Out</p>
+              </div>
+            </>
+          ) : (
+            <>
+              <li
+                className="text-purple-700 duration-150 opacity-75 cursor-pointer hover:opacity-100"
+                onClick={open}
+              >
+                Sign In
+              </li>
+              <li className="text-purple-700 duration-150 opacity-75 hover:opacity-100">
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
       <Dialog
