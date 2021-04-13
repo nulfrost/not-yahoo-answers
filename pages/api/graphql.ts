82eb8a330fc2e48f35ddbc2f742e7f61c38d03fe
Danex2 <khadane.miller@gmail.com>
2021-04-12 23:26:49 -0400

    fix: finally got cors to work for the graphql endpoint

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,11 +1,13 @@
 import schema from "../../schema/schema";
 import { context } from "schema/context";
 import { ApolloServer } from "apollo-server-micro";
+import Cors from "micro-cors";
+
+const cors = Cors();
 
 const server = new ApolloServer({
   schema,
   context,
-  debug: true,
 });
 
 export const config = {
@@ -14,6 +16,11 @@ export const config = {
   },
 };
 
-export default server.createHandler({
-  path: "/api/graphql",
+export default cors((req, res) => {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
+  return server.createHandler({ path: "/api/graphql" })(req, res);
 });
