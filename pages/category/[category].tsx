8acffb06252481201827b966326b8fd155c29ce4
Danex2 <khadane.miller@gmail.com>
2021-04-14 14:07:49 -0400

    refactor: can now view questions in their respective categories

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -3,11 +3,17 @@ import { useCategoryQuestionsQuery } from "generated/graphql";
 import { useRouter } from "next/router";
 import { LoadingSkeleton } from "components/LoadingSkeleton";
 import { QuestionCard } from "components/QuestionCard";
+import { Categories } from "components/Categories";
+import { useAllCategoriesQuery } from "generated/graphql";
+import { UserCard } from "components/UserCard";
+import { HiChevronUp } from "react-icons/hi";
+import { useState } from "react";
 
 const Category = () => {
   const router = useRouter();
+  const [toTop, setToTop] = useState(false);
   const {
-    data: { category: { questions } } = {},
+    data: { category: { questions } = {} } = {},
     loading,
   } = useCategoryQuestionsQuery({
     variables: {
@@ -17,19 +23,55 @@ const Category = () => {
     },
   });
 
+  const { data: { categories } = {} } = useAllCategoriesQuery();
+
   return (
     <Layout title={`${router.query.category}`}>
-      <div className="flex-1 max-w-6xl mx-auto mt-[88px]">
-        {loading ? (
-          <>
-            <LoadingSkeleton />{" "}
-          </>
-        ) : (
-          questions?.map((question) => {
-            return <QuestionCard key={question.id} question={question} />;
-          })
-        )}
+      <div className="flex-1 max-w-6xl mx-auto">
+        <div
+          style={{ gridTemplateColumns: "200px 1fr 200px" }}
+          className="flex flex-col h-full gap-3 xl:grid"
+        >
+          <Categories categories={categories} />
+          <div className="flex flex-col space-y-3">
+            <form className="flex flex-wrap space-y-2 xl:flex-nowrap xl:space-x-2 xl:space-y-0">
+              <input
+                type="search"
+                name="questionSearch"
+                className="w-full border border-purple-200 rounded xl:text-lg focus:outline-none focus:ring-purple-700 focus:ring-2"
+                placeholder="You've got questions, not yahoo answers has answers"
+              />
+              <select
+                name="sort"
+                id="sort"
+                className="w-full border border-purple-200 rounded xl:w-min xl:text-lg focus:outline-none focus:ring-purple-700 focus:ring-2"
+              >
+                <option value="newest">Newest</option>
+                <option value="oldest">Oldest</option>
+              </select>
+            </form>
+            {loading ? (
+              <>
+                <LoadingSkeleton />{" "}
+              </>
+            ) : (
+              questions?.map((question) => {
+                return <QuestionCard key={question.id} question={question} />;
+              })
+            )}
+          </div>
+          <UserCard />
+        </div>
       </div>
+      <HiChevronUp
+        className={`hidden xl:block fixed w-10 h-10 text-white bg-purple-600 bottom-56 right-72 transition-all duration-150 cursor-pointer ${
+          toTop ? "opacity-100" : "opacity-0"
+        }`}
+        onClick={() => {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        }}
+      />
     </Layout>
   );
 };
